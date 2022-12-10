cacdd80efda3ae87450ff8336da85a06d8cf8f5a
Henry Schimke <henry@azule.info>
2022-12-10 13:23:37 -0600

    datamatrix inverted with hint integration

---
+++ tests/InvertedDataMatrixBlackBoxTestCase.rs
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use rxing::{MultiFormatReader, DecodeHintType, DecodeHintValue};
+
+ mod common;
+
+/**
+ * Inverted barcodes
+ */
+#[test]
+  fn inverted_data_matrix_black_box_test_case() {
+    let mut tester = common::AbstractBlackBoxTestCase::new(
+      "test_resources/blackbox/inverted",
+      MultiFormatReader::default(),
+      rxing::BarcodeFormat::DATA_MATRIX,
+  );
+    // super("src/test/resources/blackbox/inverted", new MultiFormatReader(), BarcodeFormat.DATA_MATRIX);
+    tester.add_hint(DecodeHintType::ALSO_INVERTED, DecodeHintValue::AlsoInverted(true));
+    tester.add_test(1, 1, 0.0);
+    tester.add_test(1, 1, 90.0);
+    tester.add_test(1, 1, 180.0);
+    tester.add_test(1, 1, 270.0);
+
+    tester.test_black_box();
+  }
